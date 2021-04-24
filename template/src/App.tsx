d57b5d91fc21c23cf73dd4ce0b4e72343651905a
Rodrigo Lira <8973745+rodrigolira@users.noreply.github.com>
2021-04-24 18:37:22 -0300

    add and configura TailwindCss

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -4,13 +4,13 @@ import './App.css';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="text-center">
+      <header className="min-h-screen flex flex-col items-center justify-center text-white bg-coolGray-800 text-3xl">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a className="text-blue-200" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
---
+++ template/tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // or 'media' or false
+  theme: {
+    colors: colors,
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      xxl: '1400px',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
